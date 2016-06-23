1f58db9dd44ed8bb48567a7f5be2c45eb3788777
kartikadur <kartikadur@gmail.com>
2016-06-23 15:17:35 +0530

    basic visualization works. TODO: nlp to extract usable metadata, incorporate into viz

--- WebApp/js/main.js
+++ WebApp/js/main.js
@@ -1,71 +1,87 @@
 (function(window, document, d3, undefined) {
-  var width = 880,
-    height = 600,
-    cellsize = 35,
-    border = 1;
+
+  var height = 36,
+	  width = 960,
+	  margin = {top: 10, right: 10, bottom: 10, left: 10},
+	  cell = 30;
+
+	 var dateFormat = d3.time.format('%d/%m/%Y');
 
   var color = d3.scale.category20();
 
-  var svg = d3.select('body')
-              .append('svg')
-              .attr({
-                'width': width,
-                'height': height
-              });
+  var svg = d3.select('body').selectAll('.season')
+  						.data(d3.range(1, 16))
+  						.enter()
+  						.append('svg')
+  						.attr({
+  							'class' : 'season',
+  							'width' : width,
+  							'height' : height
+  						});
 
-  d3.csv('data/csvOutput.csv', function(error, csv) {
+  var tooltip = d3.select('body')
+  						.append('div')
+  						.attr({
+  							'id' : 'tooltip',
+  						})
+  						.style({
+  							'position' : 'absolute',
+  							'visibility' : 'hidden',
+  							'z-index' : 10,
+  							'margin' : '5px',
+  							'padding' : '5px'
+  						});
 
+  d3.csv("data/csvOutput.csv", function(error, csv) {
 
-    var episode = svg.selectAll('.episode')
-              .data(csv)
-              .enter()
-              .append('g')
-              .attr({
-                'class': 'episode',
-                'x': function(d) { return d.seasonEpisodeNumber * cellsize + cellsize;},
-                'y': function(d) { return d.seasonNumber * cellsize;},
-              });
+    	var data = d3.nest()
+    					.key(function(d) {return d.seasonNumber; })
+    					.map(csv);
 
+    	var yAxisText = svg.append('text')
+    					.attr({
+    						'class' : 'seasonID',
+    						'x' : cell / 2,
+    						'y' : 0.6 * height
+    					})
+    					.text(function(d) { return d; })
 
-      episode.append('rect')
-              .attr({
-                'width': cellsize,
-                'height': cellsize,
-                'x': function(d) { return d.seasonEpisodeNumber * cellsize + cellsize;},
-                'y': function(d) { return d.seasonNumber * cellsize;},
-                'fill': function(d) { return color(d.seasonNumber); },
-                'stroke-width': border,
-                'stroke': 'whitesmoke',
-                'title' : function(d) {return d.showTitle;}
-              });
+    	var episodes = svg.selectAll('.episodes')
+							.data(function(d) { return data[d]; })
+							.enter()
+							.append('rect')
+							.attr({
+								'class' : 'episodes',
+								'x' : function(d) { return d.seasonEpisodeNumber * cell; },
+								'y' : function(d) { return (height - cell) / 2;},
+								'width' : cell,
+								'height' : cell
+							})
+							.on('mouseover', function(d) {
+								pointer = d3.event
+								tooltip.style('visibility', 'visible');
+								
+								tooltip.transition()
+										.duration(200)
+										.style('opacity', 0.95)
 
-      episode.append('text')
-              .attr({
-                'class' : 'title',
-                'x' : function(d) { return d.seasonEpisodeNumber * cellsize + 1.5 * cellsize; },
-                'y' : function(d) { return d.seasonNumber * cellsize + 0.6 * cellsize; }
-              })
-              .text(function(d) { return d.seasonEpisodeNumber; });
+								tooltip.html(
+									'<h3>Title: ' + d.showTitle + '</h3>'
+									+ '<p> Original Air Date: ' + dateFormat(new Date(d.airDate)) + '</p>'
+									)
+										.style({
+											'left' : pointer.pageX + 25 + 'px',
+											'top' : pointer.pageY + 'px'
+										});
 
-
-      svg.selectAll('.y-axis')
-              .data(d3.range(0,16))
-              .enter()
-              .append('text')
-              .attr({
-                'class': 'y-axis',
-                'x': function(d) { return cellsize; },
-                'y': function(d) { return d * cellsize; },
-                'dy': function(d) { return 0.65 * cellsize; }
-              })
-              .text(function(d) { 
-                if(d === 0){
-                  return 'Season';
-                } else {
-                  return d;
-                }});
-
-
+							})
+							.on('mouseout', function(d) {
+								tooltip.transition()
+										.duration(150)
+										.style({
+											'opacity' : 0
+										});
+							});
   });
 
-})(window, document, d3);
+})(window, document, d3)
