f6074d6612f77e3480665f6996344c632d576837
kartikadur <kartikadur@gmail.com>
2016-06-25 18:03:03 +0530

    updated data source and vizualization for part 1: TODO part 2 of visuaization

--- WebApp/js/main.js
+++ WebApp/js/main.js
@@ -32,80 +32,168 @@
   							'padding' : '5px'
   						});
 
-  d3.csv("data/csvOutput.csv", function(error, csv) {
+  	d3.json('data/goodEatsJSONData.json', function(error, json) {
 
-    	var data = d3.nest()
-    					.key(function(d) {return d.seasonNumber; })
-    					.map(csv);
+  		var yAxisText = svg.append('text')
+				.attr({
+					'class' : 'seasonID',
+					'x' : cell / 2,
+					'y' : 0.6 * height
+				})
+				.text(function(d) { return d; })
 
-    	var yAxisText = svg.append('text')
-    					.attr({
-    						'class' : 'seasonID',
-    						'x' : cell / 2,
-    						'y' : 0.6 * height
-    					})
-    					.text(function(d) { return d; })
+		var episodes = svg.selectAll('.episodes')
+				.data(function(d) { return json[d]; })
+				.enter()
+				.append('rect')
+				.attr({
+					'class' : 'episodes',
+					'x' : function(d) { return d.seasonEpisodeNumber * cell; },
+					'y' : function(d) { return (height - cell) / 2;},
+					'width' : cell,
+					'height' : cell
+				})
+				.classed('active', true)
+				.on('mouseover', function(d) {
+					pointer = d3.event
+					tooltip.style('visibility', 'visible');
 
-    	var episodes = svg.selectAll('.episodes')
-							.data(function(d) { return data[d]; })
-							.enter()
-							.append('rect')
-							.attr({
-								'class' : 'episodes',
-								'x' : function(d) { return d.seasonEpisodeNumber * cell; },
-								'y' : function(d) { return (height - cell) / 2;},
-								'width' : cell,
-								'height' : cell
-							})
-							.classed('active', true)
-							.on('mouseover', function(d) {
-								pointer = d3.event
-								tooltip.style('visibility', 'visible');
+					rString = '';
+					d.recipeList.forEach(function(item) {
+						rString += '<li>' + item + '</li>';
+					});
 
-								tooltip.html(
-												'<h3>Title: ' + d.showTitle + '</h3>'
-												+ '<p> Original Air Date: ' + dateFormat(new Date(d.airDate)) + '</p>'
-										)
+					tString = ''
+					d.topicList.forEach(function(item){
+						tString += item + ', '
+					});
 
-								tooltip.style({
-									'left' : function(d) { 
-											if (tooltip[0][0].clientWidth + pointer.pageX + 50 >= window.innerWidth){
-												return pointer.pageX - tooltip[0][0].clientWidth - 40 + 'px';
-											} else {
-												return pointer.pageX + 25 + 'px'; 
-											}											
-										},
-									'top' : function(d) {
-											if (tooltip[0][0].clientHeight + pointer.pageY + 20 >= window.innerHeight){
-												return pointer.pageY - tooltip[0][0].clientHeight + 'px';
-											} else {
-												return pointer.pageY - 25 + 'px'; 
-											}
-											pointer.pageY + 'px'
-										}
-								});
-								
-								tooltip.transition()
-										.duration(250)
-										.style({
-											'opacity': 0.95
-										});
+					tString = tString.substring(0, tString.length - 2)
 
-								
+					tooltip.html(
+									'<h3>Title: ' + d.showTitle + '</h3>'
+									+ '<p> Original Air Date: ' + dateFormat(new Date(d.airDate)) + '</p>'
+									+ '<ol>' + rString + '</ol>'
+									+ '<p>' + tString + '</p>'
+							)
 
-							})
-							.on('mouseout', function(d) {
-								tooltip.transition()
-										.duration(250)
-										.style({
-											'opacity' : 0
-										});
-
-								tooltip.style({
-									'left' : '-500px',
-									'top' : '-500px'
-								});
+					tooltip.style({
+						'left' : function(d) { 
+								if (tooltip[0][0].clientWidth + pointer.pageX + 50 >= window.innerWidth){
+									return pointer.pageX - tooltip[0][0].clientWidth - 40 + 'px';
+								} else {
+									return pointer.pageX + 25 + 'px'; 
+								}											
+							},
+						'top' : function(d) {
+								if (tooltip[0][0].clientHeight + pointer.pageY + 20 >= window.innerHeight){
+									return pointer.pageY - tooltip[0][0].clientHeight + 'px';
+								} else {
+									return pointer.pageY - 25 + 'px'; 
+								}
+								pointer.pageY + 'px'
+							}
+					});
+					
+					tooltip.transition()
+							.duration(250)
+							.style({
+								'opacity': 0.95
 							});
-  });
+
+					
+
+				})
+				.on('mouseout', function(d) {
+					tooltip.transition()
+							.duration(250)
+							.style({
+								'opacity' : 0
+							});
+
+					tooltip.style({
+						'left' : '-500px',
+						'top' : '-500px'
+					});
+				});
+
+
+
+  	});
+  // 	d3.csv("data/csvOutput.csv", function(error, csv) {
+
+  //   	var data = d3.nest()
+  //   					.key(function(d) {return d.seasonNumber; })
+  //   					.map(csv);
+
+  //   	var yAxisText = svg.append('text')
+  //   					.attr({
+  //   						'class' : 'seasonID',
+  //   						'x' : cell / 2,
+  //   						'y' : 0.6 * height
+  //   					})
+  //   					.text(function(d) { return d; })
+
+  //   	var episodes = svg.selectAll('.episodes')
+		// 					.data(function(d) { return data[d]; })
+		// 					.enter()
+		// 					.append('rect')
+		// 					.attr({
+		// 						'class' : 'episodes',
+		// 						'x' : function(d) { return d.seasonEpisodeNumber * cell; },
+		// 						'y' : function(d) { return (height - cell) / 2;},
+		// 						'width' : cell,
+		// 						'height' : cell
+		// 					})
+		// 					.classed('active', true)
+		// 					.on('mouseover', function(d) {
+		// 						pointer = d3.event
+		// 						tooltip.style('visibility', 'visible');
+
+		// 						tooltip.html(
+		// 										'<h3>Title: ' + d.showTitle + '</h3>'
+		// 										+ '<p> Original Air Date: ' + dateFormat(new Date(d.airDate)) + '</p>'
+		// 								)
+
+		// 						tooltip.style({
+		// 							'left' : function(d) { 
+		// 									if (tooltip[0][0].clientWidth + pointer.pageX + 50 >= window.innerWidth){
+		// 										return pointer.pageX - tooltip[0][0].clientWidth - 40 + 'px';
+		// 									} else {
+		// 										return pointer.pageX + 25 + 'px'; 
+		// 									}											
+		// 								},
+		// 							'top' : function(d) {
+		// 									if (tooltip[0][0].clientHeight + pointer.pageY + 20 >= window.innerHeight){
+		// 										return pointer.pageY - tooltip[0][0].clientHeight + 'px';
+		// 									} else {
+		// 										return pointer.pageY - 25 + 'px'; 
+		// 									}
+		// 									pointer.pageY + 'px'
+		// 								}
+		// 						});
+								
+		// 						tooltip.transition()
+		// 								.duration(250)
+		// 								.style({
+		// 									'opacity': 0.95
+		// 								});
+
+								
+
+		// 					})
+		// 					.on('mouseout', function(d) {
+		// 						tooltip.transition()
+		// 								.duration(250)
+		// 								.style({
+		// 									'opacity' : 0
+		// 								});
+
+		// 						tooltip.style({
+		// 							'left' : '-500px',
+		// 							'top' : '-500px'
+		// 						});
+		// 					});
+  // });
 
 })(window, document, d3)
